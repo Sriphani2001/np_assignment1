91c0655532d5a38e639422d6ae3510500d43b1a2
Sri Phani Deverakonda <58811487+Sriphani2001@users.noreply.github.com>
2024-06-22 17:05:19 +0545

    Update clientmain.cpp

--- clientmain.cpp
+++ clientmain.cpp
@@ -7,183 +7,169 @@
 #include <netdb.h>
 #include <unistd.h>
 #define SA struct sockaddr
-/* You will to add includes here */
 
-// Enable if you want debugging to be printed, see example below.
-// Alternative, pass CFLAGS=-DDEBUG to make, make CFLAGS=-DDEBUG
+// Uncomment the following line to enable debugging output
 //#define DEBUG
 
-
-// Included to get the support library
+// Includes the support library for calculations
 #include <calcLib.h>
 
-// Function to receive a message from the server
-void receiveMessage(int &socket_desc, char* server_message, unsigned int msg_size){
-  memset(server_message, 0, msg_size);
-  if(recv(socket_desc, server_message, msg_size, 0) < 0){
-    #ifdef DEBUG
-    printf("Error receiving message\n");
-    #endif
-    exit(-1);
-  } else {
-    #ifdef DEBUG
-    printf("Received: %s", server_message);
-    #endif
-  }
-}
-
-// Function to send a message to the server
-void sendMessage(int &socket_desc, const char* client_message){
-  if(send(socket_desc, client_message, strlen(client_message), 0) < 0){
-    #ifdef DEBUG
-    printf("Unable to send message\n");
-    #endif
-    exit(-1);
-  } else {
-    #ifdef DEBUG
-    printf("Sent: %s", client_message);
-    #endif
-  }
-}
-
-// Function to calculate and send the result back to the server
-void calculateMessage(char* server_message, int &socket_desc){
-  int i1, i2, iresult;
-  double f1, f2, fresult;
-  char operation[10];
-
-  sscanf(server_message, "%s %lf %lf", operation, &f1, &f2);
-
-  if(strcmp(operation, "fadd") == 0){
-    fresult = f1 + f2;
-  } else if(strcmp(operation, "fsub") == 0){
-    fresult = f1 - f2;
-  } else if(strcmp(operation, "fmul") == 0){
-    fresult = f1 * f2;
-  } else if(strcmp(operation, "fdiv") == 0){
-    fresult = f1 / f2;
-  } else {
-    i1 = (int)f1;
-    i2 = (int)f2;
-
-    if(strcmp(operation, "add") == 0){
-      iresult = i1 + i2;
-    } else if(strcmp(operation, "sub") == 0){
-      iresult = i1 - i2;
-    } else if(strcmp(operation, "mul") == 0){
-      iresult = i1 * i2;
-    } else if(strcmp(operation, "div") == 0){
-      iresult = i1 / i2;
+// Receives a message from the server and stores it in the provided buffer
+void receiveMessage(int &sock_desc, char* server_response, unsigned int buffer_size) {
+    memset(server_response, 0, buffer_size);
+    if(recv(sock_desc, server_response, buffer_size, 0) < 0) {
+        #ifdef DEBUG
+        printf("Error occurred while receiving the message\n");
+        #endif
+        exit(-1);
+    } else {
+        #ifdef DEBUG
+        printf("Received: %s", server_response);
+        #endif
     }
-  }
-
-  char response[50];
-  if(server_message[0] == 'f'){
-    sprintf(response, "%8.8g\n", fresult);
-  } else {
-    sprintf(response, "%d\n", iresult);
-  }
-
-  sendMessage(socket_desc, response);
 }
 
-int main(int argc, char *argv[]){
-  if (argc != 2) {
-    printf("Usage: %s <host:port>\n", argv[0]);
-    return -1;
-  }
-
-  // Parse host and port from argv[1]
-  char* host_port = strdup(argv[1]);
-  char* colon = strrchr(host_port, ':');
-  if (!colon) {
-    printf("Invalid format. Use <host:port>\n");
-    return -1;
-  }
-
-  *colon = '\0';
-  char* host = host_port;
-  int port = atoi(colon + 1);
-
-  printf("Host %s, and port %d.\n", host, port);
-
-  // Get address info
-  struct addrinfo hints, *serverinfo;
-  memset(&hints, 0, sizeof(hints));
-  hints.ai_family = AF_UNSPEC;
-  hints.ai_socktype = SOCK_STREAM;
-
-  if (getaddrinfo(host, colon + 1, &hints, &serverinfo) < 0) {
-    printf("Getaddrinfo error: %s\n", strerror(errno));
-    return -1;
-  }
-
-  // Create socket
-  int socket_desc = socket(serverinfo->ai_family, serverinfo->ai_socktype, serverinfo->ai_protocol);
-  if (socket_desc < 0) {
-    #ifdef DEBUG
-    printf("Unable to create socket\n");
-    #endif
-    return -1;
-  }
-  #ifdef DEBUG
-  printf("Socket created\n");
-  #endif
-
-  // Connect to server
-  if (connect(socket_desc, serverinfo->ai_addr, serverinfo->ai_addrlen) < 0) {
-    #ifdef DEBUG
-    printf("Unable to connect\n");
-    #endif
-    return -1;
-  }
-  #ifdef DEBUG
-  printf("Connected\n");
-  #endif
-
-  // Free address info
-  freeaddrinfo(serverinfo);
-
-  // Receive protocol versions
-  char server_message[2000];
-  receiveMessage(socket_desc, server_message, sizeof(server_message));
-
-  // Check supported protocols
-  char* supported_protocols[10];
-  int protocol_count = 0;
-  char* line = strtok(server_message, "\n");
-  while (line != NULL) {
-    if (strcmp(line, "") == 0) break;
-    supported_protocols[protocol_count++] = line;
-    line = strtok(NULL, "\n");
-  }
-
-  int protocol_supported = 0;
-  for (int i = 0; i < protocol_count; i++) {
-    if (strcmp(supported_protocols[i], "TEXT TCP 1.0") == 0) {
-      protocol_supported = 1;
-      break;
+// Sends a message to the server
+void sendMessage(int &sock_desc, const char* client_request) {
+    if(send(sock_desc, client_request, strlen(client_request), 0) < 0) {
+        #ifdef DEBUG
+        printf("Error occurred while sending the message\n");
+        #endif
+        exit(-1);
+    } else {
+        #ifdef DEBUG
+        printf("Sent: %s", client_request);
+        #endif
     }
-  }
+}
 
-  if (protocol_supported) {
-    sendMessage(socket_desc, "OK\n");
-  } else {
-    close(socket_desc);
-    return -1;
-  }
+// Computes the result based on the server's request and sends it back
+void computeAndSendResult(char* server_response, int &sock_desc) {
+    int int1, int2, int_result;
+    double float1, float2, float_result;
+    char operation[10];
 
-  // Receive the assignment
-  receiveMessage(socket_desc, server_message, sizeof(server_message));
+    sscanf(server_response, "%s %lf %lf", operation, &float1, &float2);
 
-  // Calculate and send the result
-  calculateMessage(server_message, socket_desc);
+    if(strcmp(operation, "fadd") == 0) {
+        float_result = float1 + float2;
+    } else if(strcmp(operation, "fsub") == 0) {
+        float_result = float1 - float2;
+    } else if(strcmp(operation, "fmul") == 0) {
+        float_result = float1 * float2;
+    } else if(strcmp(operation, "fdiv") == 0) {
+        float_result = float1 / float2;
+    } else {
+        int1 = (int)float1;
+        int2 = (int)float2;
 
-  // Receive and print the final response
-  receiveMessage(socket_desc, server_message, sizeof(server_message));
-  printf("%s", server_message);
+        if(strcmp(operation, "add") == 0) {
+            int_result = int1 + int2;
+        } else if(strcmp(operation, "sub") == 0) {
+            int_result = int1 - int2;
+        } else if(strcmp(operation, "mul") == 0) {
+            int_result = int1 * int2;
+        } else if(strcmp(operation, "div") == 0) {
+            int_result = int1 / int2;
+        }
+    }
 
-  // Close socket and exit
-  close(socket_desc);
-  return 0;
+    char response[50];
+    if(server_response[0] == 'f') {
+        sprintf(response, "%8.8g\n", float_result);
+    } else {
+        sprintf(response, "%d\n", int_result);
+    }
+
+    sendMessage(sock_desc, response);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s <host:port>\n", argv[0]);
+        return -1;
+    }
+
+    char* host_port = strdup(argv[1]);
+    char* colon = strrchr(host_port, ':');
+    if (!colon) {
+        printf("Incorrect format. Use <host:port>\n");
+        return -1;
+    }
+
+    *colon = '\0';
+    char* hostname = host_port;
+    int port_number = atoi(colon + 1);
+
+    printf("Hostname: %s, Port: %d.\n", hostname, port_number);
+
+    struct addrinfo hints, *server_info;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    if (getaddrinfo(hostname, colon + 1, &hints, &server_info) < 0) {
+        printf("Error in getaddrinfo: %s\n", strerror(errno));
+        return -1;
+    }
+
+    int sock_desc = socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol);
+    if (sock_desc < 0) {
+        #ifdef DEBUG
+        printf("Socket creation failed\n");
+        #endif
+        return -1;
+    }
+    #ifdef DEBUG
+    printf("Socket successfully created\n");
+    #endif
+
+    if (connect(sock_desc, server_info->ai_addr, server_info->ai_addrlen) < 0) {
+        #ifdef DEBUG
+        printf("Connection to server failed\n");
+        #endif
+        return -1;
+    }
+    #ifdef DEBUG
+    printf("Successfully connected to server\n");
+    #endif
+
+    freeaddrinfo(server_info);
+
+    char server_response[2000];
+    receiveMessage(sock_desc, server_response, sizeof(server_response));
+
+    char* supported_protocols[10];
+    int protocol_count = 0;
+    char* line = strtok(server_response, "\n");
+    while (line != NULL) {
+        if (strcmp(line, "") == 0) break;
+        supported_protocols[protocol_count++] = line;
+        line = strtok(NULL, "\n");
+    }
+
+    int protocol_supported = 0;
+    for (int i = 0; i < protocol_count; i++) {
+        if (strcmp(supported_protocols[i], "TEXT TCP 1.0") == 0) {
+            protocol_supported = 1;
+            break;
+        }
+    }
+
+    if (protocol_supported) {
+        sendMessage(sock_desc, "OK\n");
+    } else {
+        close(sock_desc);
+        return -1;
+    }
+
+    receiveMessage(sock_desc, server_response, sizeof(server_response));
+
+    computeAndSendResult(server_response, sock_desc);
+
+    receiveMessage(sock_desc, server_response, sizeof(server_response));
+    printf("%s", server_response);
+
+    close(sock_desc);
+    return 0;
 }
